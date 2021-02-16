b2536b64652e0a67c43755b9365f66f3af74dfba
Yuri Ershov <shtorm0606@gmail.com>
2021-02-16 15:57:33 +0300

    Initial Commit

---
+++ SwiftUI-Videos-SeanAllen/SwiftUI_Videos_SeanAllenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Videos_SeanAllenApp.swift
+//  SwiftUI-Videos-SeanAllen
+//
+//  Created by Yuri Ershov on 16.02.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Videos_SeanAllenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
